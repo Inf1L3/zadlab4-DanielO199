70c01e522e94c086ac73e24eb1644e9f8d1c918a
piotrjasiu95 <piotrjasiu95@gmail.com>
2018-03-21 17:31:05 +0100

    Zadania 1,2,3

--- POLab4/src/main/java/pl/edu/ur/polab4/Main.java
+++ POLab4/src/main/java/pl/edu/ur/polab4/Main.java
@@ -1,12 +1,35 @@
 package pl.edu.ur.polab4;
 
+import java.util.Scanner;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
+        
+          System.out.println("Wprowadz dane");
+         Scanner imie= new Scanner(System.in);
+        System.out.println("Wprowadz imie:");
+         String a=imie.next();
+         System.out.println("Wprowadz nazwisko:");
+         String b=imie.next();
+         System.out.println("Wprowadz nr_indeksu:");
+          int c=imie.nextInt();
+          System.out.println("Wprowadz nazwe specjalizacji:");
+         String d=imie.next();
+         System.out.println("Wprowadz rok studiow:");
+         int e=imie.nextInt();
+        
+        Student osoba1 = new Student(a, b, c,d,e);
+        osoba1.pokazDane();
+        
         // TODO zad 4, 5
     }
     
 }
+
+
+
+
+
+
