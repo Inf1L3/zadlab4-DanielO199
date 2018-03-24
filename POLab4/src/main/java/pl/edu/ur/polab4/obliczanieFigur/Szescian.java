e48bd02bab7ebffdb7a9e1e01983001b02d1b866
XxX <XxX@XxX-Komputer>
2018-03-24 15:48:32 +0100

    Zadania 4,5

--- POLab4/src/main/java/pl/edu/ur/polab4/obliczanieFigur/Szescian.java
+++ POLab4/src/main/java/pl/edu/ur/polab4/obliczanieFigur/Szescian.java
@@ -1,1 +1,33 @@
+package pl.edu.ur.polab4.obliczanieFigur;
 
+public class Szescian {
+
+    public int a;
+    public int pole;
+    public int objetosc;
+
+    public Szescian(int a) {
+
+        this.a = a; 
+
+    }
+
+    public int pole(int a) {
+        pole = (a * a) * 6;
+        return pole;
+
+    }
+
+    public int objetosc(int a) {
+
+        objetosc = a * a * a;
+        return objetosc;
+    }
+
+    public void Pokadane() {
+        System.out.println(" Bok szescianu to: " + this.a);
+        System.out.println("Pole szescianu = " + pole(a));
+        System.out.println("Objetosc szescianu " + objetosc(a));
+
+    }
+}
